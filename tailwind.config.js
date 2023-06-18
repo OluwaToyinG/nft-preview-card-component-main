080baf138853c097cc61fcedbe64e245f70ef71d
OluwaToyin Agboola <115198993+OluwaToyinG@users.noreply.github.com>
2023-06-18 23:06:35 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        'soft-blue': '#8BACDA',
+        cyan: '#00FFF7',
+        'very-dark-blue-main': '#0D192B',
+        'very-dark-blue-card': '#14253D',
+        'very-dark-blue-line': '#2F415B',
+        white: '#FFFFFF',
+      },
+fontFamily: {
+  outfit: ['Outfit', 'sans-serif'],
+},
+      
+
+  },
+  
+},
+plugins: [],
+}
+
+
